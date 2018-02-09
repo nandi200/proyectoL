520fcac058434a41d32d2fc8013f9a0a70c1e309
erandi <ecastillom@ipn.mx>
2018-02-09 10:40:18 -0600

    proyectoL

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,30 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Home';
+});
+
+Route::get('/usuarios', function (){
+    return 'Usuarios';    
+});
+
+Route::get('/usuarios/nuevo', function (){
+ return 'Crear nuevo usuario';
+});
+
+Route::get('/usuarios/{id}',function($id){
+    
+   return "Mostrando detalle del usuario:{$id}"; 
+    
+});
+
+Route::get('/saludo/{name}/{nickname?}', function ($name,$nickname=null){
+  
+    if($nickname){
+    return "Bienvenido {$name}, tu apodo es {$nickname}";
+    } else{
+        
+     return "Bienvenido {$name}, no tienes apodo";    
+    }
+      
 });
